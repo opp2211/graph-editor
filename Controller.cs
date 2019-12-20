6a458ac5c271c95a8903600a027b32c4d99bb1a4
Kirill <opp.2211@gmail.com>
2019-12-20 17:42:08 +0500

    Контроллер

---
+++ Controller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphics_editor
+{
+    class Controller
+    {
+        Model model;
+        public Controller(Model model)
+        {
+            this.model = model;
+        }
+        public void ViewPort(int x0, int y0, int w, int h, Graphics graphics)
+        {
+            model.ViewPort(x0, y0, w, h, graphics);
+        }
+        public void SetCreatedObjType(int createdObjType)
+        {
+            model.SetCreatedObjType(createdObjType);
+        }
+        public void SetPenProps(int penColor, int penWidth)
+        {
+            model.SetPenProps(penColor, penWidth);
+        }
+        public void SetBrushProps(int brushColor)
+        {
+            model.SetBrushProps(brushColor);
+        }
+        public void Wipe()
+        {
+            model.Wipe();
+        }
+        public void MouseDown(int x, int y)
+        {
+
+        }
+        public void MouseUp(int x, int y)
+        {
+
+        }
+        public void MouseMove(int x, int y)
+        {
+
+        }
+    }
+}
